7f44c51c7c42529e23414a722e1353fa5a58a9d6
joe <jre2016@gmail.com>
2022-01-07 12:34:00 -0500

    i

---
+++ build/precache-manifest.885b5c3ff66326145c64702d70a13268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2144ba8abe6f8f3a39cc",
+    "url": "/static/css/main.f62c251e.chunk.css"
+  },
+  {
+    "revision": "2144ba8abe6f8f3a39cc",
+    "url": "/static/js/main.5f439aa7.chunk.js"
+  },
+  {
+    "revision": "bbb365e04cc8124cb486",
+    "url": "/static/js/runtime~main.6fc83ea1.js"
+  },
+  {
+    "revision": "eff18fa82e847b073916",
+    "url": "/static/js/2.dfaa40cc.chunk.js"
+  },
+  {
+    "revision": "e22b2cb53ed8a9a942c1",
+    "url": "/static/js/3.5cee360e.chunk.js"
+  },
+  {
+    "revision": "fccea148fc5f9b41f0479d7a6934a2c9",
+    "url": "/index.html"
+  }
+];
